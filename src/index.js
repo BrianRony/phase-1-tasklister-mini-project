0d55064030b4336d8e464d3f9bf92b5f17010f5d
Brian Ngetich <kipronobrian97@gmail.com>
2024-04-09 17:43:59 +0300

    ToDo List Functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    buildTask(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+function buildTask(task){
+  let list = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDel);
+  btn.textContent = "X";
+  list.textContent = `${task}  `
+  list.appendChild (btn)
+  document.querySelector("#tasks").appendChild(list)
+}
+
+function handleDel(e){
+  e.target.parentNode.remove();
+}
